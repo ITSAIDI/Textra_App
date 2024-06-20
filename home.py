5395a5c7fc51f99f816964ba1696f72987b92b6d
Noureddine <monojob111@gmail.com>
2024-06-20 16:21:25 +0000

    c

--- home.py
+++ home.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
 def app():
-    st.title("Home")
+    st.title("Textra")
+    st.write("This project integrates Optical Character Recognition (OCR) technology with Language Models (LM) to enhance the extraction of text from French invoices. By leveraging the capabilities of OCR to capture text from scanned documents and the contextual understanding provided by Language Models")
+    image_path = '/teamspace/studios/this_studio/Textra_App/images/Last.png'
+    image = open(image_path, 'rb').read()
+    st.image(image,width=1000)
--- main.py
+++ main.py
@@ -37,6 +37,36 @@ css = """
 
 st.markdown(css, unsafe_allow_html=True)
 
+video_html = """
+		<style>
+
+		#myVideo {
+		  position: fixed;
+		  right: 0;
+		  bottom: 0;
+		  min-width: 100%; 
+		  min-height: 100%;
+		}
+
+		.content {
+		  position: fixed;
+		  bottom: 0;
+		  background: rgba(0, 0, 0, 0.5);
+		  color: #f1f1f1;
+		  width: 100%;
+		  padding: 20px;
+		}
+
+		</style>	
+		<video autoplay muted loop id="myVideo">
+		  <source src="https://static.streamlit.io/examples/star.mp4")>
+		  Your browser does not support HTML5 video.
+		</video>
+        """
+
+#st.markdown(video_html, unsafe_allow_html=True)
+
+
 app = option_menu(
 menu_title=None,
 options=['Home','Account','Use','Chat','Your Invoices'],
