9ec69bbf0d1e513e699af5038527b835a51133c1
Noureddine <monojob111@gmail.com>
2024-06-20 16:32:20 +0000

    c

--- home.py
+++ home.py
@@ -3,7 +3,7 @@ import streamlit as st
 def app():
     st.title("Textra")
     st.write("This project integrates Optical Character Recognition (OCR) technology with Language Models (LM) to enhance the extraction of text from French invoices. By leveraging the capabilities of OCR to capture text from scanned documents and the contextual understanding provided by Language Models")
-    url = "https://raw.githubusercontent.com/ITSAIDI/Textra_App/main/images/Last.png?token=GHSAT0AAAAAACPS7L5WDUTBEJWTKTLYZK4UZTULBVA"
+    url = "https://raw.githubusercontent.com/ITSAIDI/Textra_App/main/images/Last.png?token=GHSAT0AAAAAACPS7L5XVGVIRQXKY5M5IY7IZTULGRQ"
     st.image(url, width=1000)
 
    
